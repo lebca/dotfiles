2627063a0f6af8b8a5ccf4d07761337df7aece98
Caleb Holtzinger <lebca.caleb@gmail.com>
2018-10-27 18:24:08 -0700

    new comp setup

---
+++ new_comp_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check that Home-brew is installed and install if not
+if test ! $(which_brew)
+then
+  echo "Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+fi
+
+brew update
+brew upgrade —all
+
+brew install wget
+brew install tree
+brew install ack
+brew install vim
+
+echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+source ~/.zshrc
+echo ".dotfiles.git" >> .gitignore
+git clone --bare https://www.github.com/lebca/repo.git $HOME/.dotfiles.git
+dotfiles checkout
+dotfiles config --local status.showUntrackedFiles no
+
+echo "dot files done"
+
+brew install iterm2
+brew cask install little-snitch
+brew cask install mpv
+brew cask install dropbox
+brew cask install slack
+
